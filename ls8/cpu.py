b64f2ce1d7a1b0cd665a4ef5c356a1a9faa1e7fe
krikaz <pascal.bou@protonmail.com>
2019-11-11 16:57:12 +0100

    test passed, prints 8, debug lines commented

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -46,8 +46,7 @@ class CPU:
         elif op == 'LDI':
             self.reg[reg_a] = reg_b
         elif op == 'PRN':
-            self.reg[reg_a] = reg_a
-            print('printing PRN')
+            # print(f'self.reg[reg_a] {self.reg[reg_a]}')
             print(self.reg[reg_a])
         elif op == 'HLT':
             self.running = False
@@ -89,11 +88,12 @@ class CPU:
 
         while self.running:
             command = self.ram[self.pc]
-            print(f'self.pc {self.pc}')
+            # print(f'self.pc {self.pc}')
             command_string = format(command, '#010b')
-            print(f'command_string {command_string}')
+            # print(f'command_string {command_string}')
             instruction_bits = command_string[2:4]
-            print(f'instruction_bits {instruction_bits}')
+            # print(f'instruction_bits {instruction_bits}')
+
             if instruction_bits == '10':
                 operand_a = self.ram_read(self.pc + 1)
                 operand_b = self.ram_read(self.pc + 2)   
@@ -104,22 +104,23 @@ class CPU:
                 operand_a = None
                 operand_b = None
 
-            print(f'operand_a {operand_a}')
-            print(f'operand_b {operand_b}')
+            # print(f'operand_a {operand_a}')
+            # print(f'operand_b {operand_b}')
 
             if command_string == '0b10000010':
                 op = 'LDI'
-                self.pc += 3
+                instruction_size = 3
             elif command_string == '0b01000111':
                 op = 'PRN'
-                self.pc += 2
+                instruction_size = 2
             elif command_string == '0b00000001':
                 op = 'HLT'
-                self.pc = 0
 
 
-            print(f'op {op}')
+            # print(f'op {op}')
             self.alu(op, operand_a, operand_b)
-            time.sleep(1)
+            self.pc += instruction_size
+            # time.sleep(1)
+
             
             
