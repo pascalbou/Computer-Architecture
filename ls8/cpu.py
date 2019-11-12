9cb6c9ad40db18416b77d5e99046e7c547bba29e
krikaz <pascal.bou@protonmail.com>
2019-11-12 16:25:34 +0100

    cleaned comments for load()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -18,25 +18,6 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        # address = 0
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     # print(self.ram[address])
-        #     address += 1
-
         if len(sys.argv) != 2:
             print("usage: 02-fileio02.py <filename>")
             sys.exit(1)
@@ -60,11 +41,9 @@ class CPU:
 
                     # set the number to an integer of base 2
                     instruction = int(num, 2)
-                    # print the value in binary and in decimal
-                    # print(f"{value:08b}: {value:d}")
 
+                    # loads instruction in ram
                     self.ram[address] = instruction
-                    # print(self.ram[address])
                     address += 1
 
         except FileNotFoundError:
