1f2132ab55d1a4b1624d03e33d3ff95ce9d84bf0
krikaz <pascal.bou@protonmail.com>
2019-11-15 10:53:17 +0100

    refactored with global variables, all mvp tests passed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,12 +1,13 @@
 """CPU functionality."""
 
-import sys, time
+import sys
+import time
 
 # global variables
 # register
-IM = 5 # interrupt mask
-IS = 6 # interrupt status
-SP = 7 # stack pointer
+IM = 5  # interrupt mask
+IS = 6  # interrupt status
+SP = 7  # stack pointer
 
 # opcode
 ADD = 0b10100000
@@ -22,7 +23,7 @@ CMP = 0b10100111
 JMP = 0b01010100
 JEQ = 0b01010101
 JNE = 0b01010110
-ADDI = 0b10100001 # coded invented, not in doc
+ADDI = 0b10100001  # coded invented, not in doc
 AND = 0b10101000
 OR = 0b10101010
 NOT = 0b01101001
@@ -32,26 +33,50 @@ MOD = 0b10100100
 XOR = 0b10101011
 ST = 0b10000100
 
+instrution_size_table = {
+    ADD: 3,
+    LDI: 3,
+    PRN: 2,
+    HLT: 0,
+    MUL: 3,
+    PUSH: 2,
+    POP: 2,
+    CALL: 2,
+    RET: 0,
+    JMP: 2,
+    CMP: 3,
+    JEQ: 2,
+    JNE: 2,
+    ADDI: 2,
+    AND: 3,
+    OR: 3,
+    NOT: 2,
+    SHL: 3,
+    SHR: 3,
+    MOD: 3,
+    XOR: 3,
+    ST: 3,
+}
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256 # random access memory
-        self.reg = [0] * 8 # registers
-        self.reg[SP] = 0xF4 # Stack Pointer
+        self.ram = [0] * 256  # random access memory
+        self.reg = [0] * 8  # registers
+        self.reg[SP] = 0xF4  # Stack Pointer
 
         # internal registers
-        self.pc = 0 # program counter
-        self.ir = '' # Instruction Register
-        self.mar = 0 # Memory Address Register
-        self.mdr = 0 # Memory Data Register
-        self.fl = 0b000 # flag
+        self.pc = 0  # program counter
+        self.ir = ''  # Instruction Register
+        self.mar = 0  # Memory Address Register
+        self.mdr = 0  # Memory Data Register
+        self.fl = 0b000  # flag
 
-        self.running = True # is the program running?
+        self.running = True  # is the program running?
         self.branch_table = {}
-        
 
     def load(self):
         """Load a program into memory."""
@@ -91,23 +116,23 @@ class CPU:
     def alu(self, op, reg_a=None, reg_b=None):
         """ALU operations."""
 
-        if op == "ADD":
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == 'LDI':
+        elif op == LDI:
             self.reg[reg_a] = reg_b
-        elif op == 'PRN':
+        elif op == PRN:
             print(self.reg[reg_a])
-        elif op == 'HLT':
+        elif op == HLT:
             self.running = False
-        elif op == 'MUL':
+        elif op == MUL:
             self.reg[reg_a] *= self.reg[reg_b]
-        elif op == 'PUSH':
-            self.reg[SP] -= 1 # stack pointer
+        elif op == PUSH:
+            self.reg[SP] -= 1  # stack pointer
             self.ram[self.reg[SP]] = self.reg[reg_a]
-        elif op == 'POP':
+        elif op == POP:
             self.reg[reg_a] = self.ram[self.reg[SP]]
-            self.reg[SP] += 1 # stack pointer
-        elif op == 'CMP':
+            self.reg[SP] += 1  # stack pointer
+        elif op == CMP:
             if self.reg[reg_a] == self.reg[reg_b]:
                 self.fl = 0b001
             # elif self.reg[reg_a] > self.reg[reg_b]:
@@ -116,24 +141,24 @@ class CPU:
             #     self.fl = 0b100
             else:
                 self.fl = 0b000
-        elif op == 'ADDI':
+        elif op == ADDI:
             self.reg[reg_a] += reg_b
-        elif op == 'AND':
+        elif op == AND:
             self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
-        elif op == 'OR':
+        elif op == OR:
             self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
-        elif op == 'NOT':
+        elif op == NOT:
             self.reg[reg_a] = ~(self.reg[reg_a])
-        elif op == 'SHL':
+        elif op == SHL:
             self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
-        elif op == 'SHR':
+        elif op == SHR:
             self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
-        elif op == 'MOD':
+        elif op == MOD:
             if self.reg[reg_b] == 0:
                 raise Exception("Cannot divide by 0")
             else:
                 self.reg[reg_a] = self.reg[reg_a] % self.reg[reg_b]
-        elif op == 'ST':
+        elif op == ST:
             self.reg[reg_a] = self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
@@ -146,8 +171,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -173,118 +198,66 @@ class CPU:
 
         while self.running:
             # get command saved in ram
-            command = self.ram[self.pc]
-            # string binary command
-            command_string = format(command, '#010b')
+            self.ir = self.ram[self.pc]
+
             # get the first two bits, the instruction_bits
-            instruction_bits = command_string[2:4]
+            instruction_bits = bin(self.ir)[2:4]
 
             # saved one or two operands
             if instruction_bits == '10':
                 operand_a = self.ram_read(self.pc + 1)
-                operand_b = self.ram_read(self.pc + 2)   
+                operand_b = self.ram_read(self.pc + 2)
             elif instruction_bits == '01':
-                operand_a = self.ram_read(self.pc + 1)  
+                operand_a = self.ram_read(self.pc + 1)
 
-            op_table = {
-                '0b10100000': 'ADD',
-                '0b10000010': 'LDI',
-                '0b01000111': 'PRN',
-                '0b00000001': 'HLT',
-                '0b10100010': 'MUL',
-                '0b01000101': 'PUSH',
-                '0b01000110': 'POP',
-                '0b01010000': 'CALL',
-                '0b00010001': 'RET',
-                '0b10100111': 'CMP',
-                '0b01010100': 'JMP',
-                '0b01010101': 'JEQ',
-                '0b01010110': 'JNE',
-                '0b10100001': 'ADDI', # coded invented, not in doc
-                '0b10101000': 'AND',
-                '0b10101010': 'OR',
-                '0b01101001': 'NOT',
-                '0b10101100': 'SHL',
-                '0b10101101': 'SHR',
-                '0b10100100': 'MOD',
-                '0b10101011': 'XOR',
-                '0b10000100': 'ST',
-            }
+            instructions_that_set_pc = [
+                CALL, RET, JMP, JEQ, JNE, XOR]
 
-            instrution_size_table = {
-                'ADD': 3,
-                'LDI': 3,
-                'PRN': 2,
-                'HLT': 0,
-                'MUL': 3,
-                'PUSH': 2,
-                'POP': 2,
-                'CALL': 2,
-                'RET': 0,
-                'JMP': 2,
-                'CMP': 3,
-                'JEQ': 2,
-                'JNE': 2,
-                'ADDI': 2,
-                'AND': 3,
-                'OR': 3,
-                'NOT': 2,
-                'SHL': 3,
-                'SHR': 3,
-                'MOD': 3,
-                'XOR': 3,
-                'ST': 3,
-            }
-
-            instructions_that_set_pc = ['CALL', 'RET', 'JMP', 'JEQ', 'JNE', 'XOR']
-
-            # get name of operation
-            self.ir = op_table[command_string]
             # get size of operation instrutions
             instruction_size = instrution_size_table[self.ir]
 
             # if it is an instruction that sets the pc
             if self.ir in instructions_that_set_pc:
-                if self.ir == 'CALL':
+                if self.ir == CALL:
                     self.reg[SP] = self.pc + 2
-                    self.alu('PUSH', 7)
+                    self.alu(PUSH, 7)
                     self.pc = self.reg[operand_a]
-                elif self.ir == 'RET':
-                    self.alu('POP', 7)
+                elif self.ir == RET:
+                    self.alu(POP, 7)
                     self.pc = self.reg[SP]
-                elif self.ir == 'JEQ':
+                elif self.ir == JEQ:
                     if self.fl == 0b001:
-                        self.ir = 'JMP'
+                        self.ir = JMP
                     else:
                         self.pc += instruction_size
-                elif self.ir == 'JNE':
+                elif self.ir == JNE:
                     if self.fl == 0b000:
-                        self.ir = 'JMP'
+                        self.ir = JMP
                     else:
                         self.pc += instruction_size
-                elif self.ir == 'XOR':
+                elif self.ir == XOR:
                     # XOR = ((OR) AND (NAND))
                     operand_c = operand_a + 2
                     self.reg[operand_c] = self.reg[operand_a]
-                    self.alu('OR', operand_a, operand_b)
-                    self.alu('AND', operand_c, operand_b)
-                    self.alu('NOT', operand_c,)
-                    self.alu('AND', operand_a, operand_c)
+                    self.alu(OR, operand_a, operand_b)
+                    self.alu(AND, operand_c, operand_b)
+                    self.alu(NOT, operand_c,)
+                    self.alu(AND, operand_a, operand_c)
                 # extras ALU gates
-                # elif self.ir == 'NAND':
+                # elif self.ir == NAND:
                 #     # NAND = NOT(AND)
-                #     self.alu('AND', operand_a, operand_b)
-                #     self.alu('NOT', operand_a)   
-                # elif self.ir == 'NOR':
-                #     # NOR = ((XOR) XOR (NAND))  
+                #     self.alu(AND, operand_a, operand_b)
+                #     self.alu(NOT, operand_a)
+                # elif self.ir == NOR:
+                #     # NOR = ((XOR) XOR (NAND))
                 #     operand_c = operand_a + 2
                 #     self.reg[operand_c] = self.reg[operand_a]
-                #     self.alu('XOR', operand_a, operand_b)
-                #     self.alu('AND', operand_c, operand_b)
-                #     self.alu('NOT', operand_c,)
-                #     self.alu('XOR', operand_a, operand_c)                    
-    
-                if self.ir == 'JMP':
+                #     self.alu(XOR, operand_a, operand_b)
+                #     self.alu(AND, operand_c, operand_b)
+                #     self.alu(NOT, operand_c,)
+                #     self.alu(XOR, operand_a, operand_c)
+
+                if self.ir == JMP:
                     self.pc = self.reg[operand_a]
             else:
                 # move to next operation
@@ -292,10 +265,3 @@ class CPU:
                 # execute ALU method
                 self.alu(self.ir, operand_a, operand_b)
 
-            # == Debugging ==
-            # time.sleep(1)
-            # print(self.ir)
-            # print(operand_a)
-            # print(operand_b)
-            # print(self.reg)
-            # print(self.fl)
